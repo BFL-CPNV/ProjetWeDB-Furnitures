00686fac650b3775f0df350486ed92d23d0d0417
nicolas.glassey <nicolas.glassey@cpnv.ch>
2020-04-26 00:09:24 +0200

    change documentation path

--- docs/html/html/controler_2articles_8php.js
+++
@@ -1,4 +0,0 @@
-var controler_2articles_8php =
-[
-    [ "displayArticles", "controler_2articles_8php.html#a0c933da33d86c5eb6532f014012b8019", null ]
-];
--- docs/html/html/dir_66954e496b09b2033707e43a9650cfac.js
+++
@@ -1,5 +0,0 @@
-var dir_66954e496b09b2033707e43a9650cfac =
-[
-    [ "articles.php", "controler_2articles_8php.html", "controler_2articles_8php" ],
-    [ "users.php", "users_8php.html", "users_8php" ]
-];
