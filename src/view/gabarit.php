4ba48e27f09cb6f3adae863c4ea9b31828ed56e8
AdamGruber <adam.gruber@cpnv.ch>
2020-05-07 15:13:53 +0200

    Banner fix

--- src/view/gabarit.php
+++ src/view/gabarit.php
@@ -17,7 +17,6 @@
     <title>aranoz</title>
 
 
-
     <link rel="icon" href="view/content/img/favicon.png" type="text/css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="view/content/css/bootstrap.min.css" type="text/css">
@@ -58,51 +57,16 @@
                     <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
-                                   role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Shop
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
-                                    <a class="dropdown-item" href="category.html"> shop category</a>
-                                    <a class="dropdown-item" href="single-product.html">product details</a>
-
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_3"
-                                   role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    pages
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="login.html"> login</a>
-                                    <a class="dropdown-item" href="tracking.html">tracking</a>
-                                    <a class="dropdown-item" href="checkout.html">product checkout</a>
-                                    <a class="dropdown-item" href="cart.html">shopping cart</a>
-                                    <a class="dropdown-item" href="confirmation.html">confirmation</a>
-                                    <a class="dropdown-item" href="elements.html">elements</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_2"
-                                   role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    blog
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="blog.html"> blog</a>
-                                    <a class="dropdown-item" href="single-blog.html">Single blog</a>
-                                </div>
+                                <a class="nav-link" href="index.php">Acceuil</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="contact.html">Contact</a>
+                                <a class="nav-link" href="index.php?action=produits">Produits</a>
                             </li>
 
                             <?php if (!isset($_SESSION['userEmailAddress']) || (!isset($_GET['action'])) || ((@$_GET['action'] == "logout"))) : ?>
-                                <li><a class="nav-link" href="index.php?action=login">Login</a></li>
-                                <li><a class="nav-link" href="index.php?action=register" style="color:#FC05CB">S'inscrire</a>
+                                <li class="nav-item"><a class="nav-link" href="index.php?action=login">Login</a></li>
+                                <li class="nav-item"><a class="nav-link" href="index.php?action=register" style="color:#FC05CB">S'inscrire</a>
                                 </li>
                             <?php else : ?>
                                 <!-- Display the button useful for logout-->
@@ -116,35 +80,7 @@
                             <?php endif; ?>
                         </ul>
                     </div>
-                    <div class="hearer_icon d-flex">
-                        <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
-                        <a href=""><i class="ti-heart"></i></a>
-                        <div class="dropdown cart">
-                            <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-cart-plus"></i>
-                            </a>
-                            <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <div class="single_product">
 
-                                </div>
-                            </div> -->
-
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </div>
-    <div class="search_input" id="search_input_box">
-        <div class="container ">
-            <form class="d-flex justify-content-between search-inner">
-                <input type="text" class="form-control" id="search_input" placeholder="Search Here">
-                <button type="submit" class="btn"></button>
-                <span class="ti-close" id="close_search" title="Close Search"></span>
-            </form>
-        </div>
-    </div>
 </header>
 <!-- Header part end-->
 
@@ -153,6 +89,7 @@
 <!-- ________ SLIDER_____________-->
 <!-- banner part start-->
 <?php if ((@$_GET['action'] == "home") || (!isset($_GET['action']))) : ?>
+    <!-- banner part start-->
     <section class="banner_part">
         <div class="container">
             <div class="row align-items-center">
@@ -172,11 +109,10 @@
                                     </div>
                                 </div>
                                 <div class="banner_img d-none d-lg-block">
-                                    <img src=view/content/img/banner_img.png" alt="">
+                                    <img src="view/content/img/banner_img.png" alt="">
                                 </div>
                             </div>
-                        </div>
-                        <div class="single_banner_slider">
+                        </div><div class="single_banner_slider">
                             <div class="row">
                                 <div class="col-lg-5 col-md-8">
                                     <div class="banner_text">
@@ -193,8 +129,7 @@
                                     <img src="view/content/img/banner_img.png" alt="">
                                 </div>
                             </div>
-                        </div>
-                        <div class="single_banner_slider">
+                        </div><div class="single_banner_slider">
                             <div class="row">
                                 <div class="col-lg-5 col-md-8">
                                     <div class="banner_text">
@@ -212,23 +147,6 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- <div class="single_banner_slider">
-                            <div class="row">
-                                <div class="col-lg-5 col-md-8">
-                                    <div class="banner_text">
-                                        <div class="banner_text_iner">
-                                            <h1>Cloth $ Wood Sofa</h1>
-                                            <p>Incididunt ut labore et dolore magna aliqua quis ipsum
-                                                suspendisse ultrices gravida. Risus commodo viverra</p>
-                                            <a href="#" class="btn_2">buy now</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="banner_img d-none d-lg-block">
-                                    <img src="img/banner_img.png" alt="">
-                                </div>
-                            </div>
-                        </div> -->
                     </div>
                     <div class="slider-counter"></div>
                 </div>
