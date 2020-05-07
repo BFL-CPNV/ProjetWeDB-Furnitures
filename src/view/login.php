47a4530d9db02ed99a0ab4557347f0a6d2fd0c10
AdamGruber <adam.gruber@cpnv.ch>
2020-05-07 15:13:21 +0200

    Login template implemented

--- src/view/login.php
+++ src/view/login.php
@@ -11,11 +11,54 @@ $title = 'Rent A Snow - Login/Logout';
 
 ob_start();
 ?>
-    <h2>Login</h2>
 <?php if(isset($loginErrorMessage)) : ?>
     <h5><span style="color:red"><?= $loginErrorMessage; ?></span></h5>
 <?php endif ?>
-    <article>
+
+    <section class="login_part padding_top">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="login_part_text text-center">
+                        <div class="login_part_text_iner">
+                            <h2>Besoin d'un compte ?</h2>
+                            <p>Cliquez sur le bouton ci-dessous</p>
+                            <a href="index.php?action=register" class="btn_3">S'inscrire</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="login_part_form">
+                        <div class="login_part_form_iner">
+                            <h3>Login</h3>
+                            <form class="row contact_form" action="index.php?action=login" method="POST" novalidate="novalidate">
+                                <div class="col-md-12 form-group p_star">
+                                    <input type="email" class="form-control" id="name" name="inputUserEmailAddress" value=""
+                                           placeholder="E-mail address" required>
+                                </div>
+                                <div class="col-md-12 form-group p_star">
+                                    <input type="password" class="form-control" id="password" name="inputUserPsw" value=""
+                                           placeholder="Password" required>
+                                </div>
+                                <div class="col-md-12 form-group">
+                                    <div class="creat_account d-flex align-items-center">
+                                        <input type="checkbox" id="f-option" name="selector">
+                                        <label for="f-option">Remember me</label>
+                                    </div>
+                                    <button type="submit" value="submit" class="btn_3">
+                                        log in
+                                    </button>
+                                    <a class="lost_pass" href="#">forget password?</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+   <!-- <article>
         <form class='form' method='POST' action="index.php?action=login">
             <div class="container">
                 <label for="userEmail"><b>Adresse email</b></label>
@@ -33,7 +76,7 @@ ob_start();
         <div class="container signin">
             <p>Besoin d'un compte <a href="index.php?action=register">S'inscrire</a>.</p>
         </div>
-    </article>
+    </article>  -->
 <?php
 $content = ob_get_clean();
 require 'gabarit.php';
