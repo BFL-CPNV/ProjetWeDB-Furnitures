536ba597da390dc885042270d3a96e8e1fe1fb63
FardelusDeletus <48351345+FardelusDeletus@users.noreply.github.com>
2020-05-14 14:24:11 +0200

    Updated doxygen doc

--- docs/html/gabarit_8php.js
+++ docs/html/gabarit_8php.js
@@ -1,5 +1,4 @@
 var gabarit_8php =
 [
-    [ "endif", "gabarit_8php.html#afbf40ab7e9cf93abeb0fd64dfb687bd2", null ],
-    [ "if", "gabarit_8php.html#ae023eada04346291851b76b5409b00bd", null ]
+    [ "endif", "gabarit_8php.html#aff32d44a2c70923a426c31d7d996edbb", null ]
 ];
--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -1,33 +1,35 @@
 /*
-@licstart  The following is the entire license notice for the
-JavaScript code in this file.
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
 
-Copyright (C) 1997-2019 by Dimitri van Heesch
+ The MIT License (MIT)
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of version 2 of the GNU General Public License as published by
-the Free Software Foundation
+ Copyright (C) 1997-2020 by Dimitri van Heesch
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
 
-@licend  The above is the entire license notice
-for the JavaScript code in this file
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var NAVTREE =
 [
-  [ "Snows", "index.html", [
-    [ "2.0 BOOTSTRAP JS PHILOSOPHY", "md__c_1__my_devs__projet_web_bdd_snows_src_view_content_documentation_assets_js__r_e_a_d_m_e.html", null ],
-    [ "README", "md__c_1__my_devs__projet_web_bdd_snows_src_view_content_scripts_fontawesome__r_e_a_d_m_e.html", null ],
-    [ "Vegas Background jQuery Plugin", "md__c_1__my_devs__projet_web_bdd_snows_src_view_content_scripts_vegas__r_e_a_d_m_e.html", null ],
-    [ "README", "md__c_1__my_devs__projet_web_bdd_snows_src_view_content_site_scripts_fontawesome__r_e_a_d_m_e.html", null ],
-    [ "Vegas Background jQuery Plugin", "md__c_1__my_devs__projet_web_bdd_snows_src_view_content_site_scripts_vegas__r_e_a_d_m_e.html", null ],
+  [ "ProjetWebBdd-Furnitures", "index.html", [
+    [ "2.0 BOOTSTRAP JS PHILOSOPHY", "md__c_1__users_basti__one_drive__documents__git_hub_projetwebbdd_startcode_2019_2020_src_view_coc2d65df82506a550d7d205d5ebf61e44.html", null ],
+    [ "README", "md__c_1__users_basti__one_drive__documents__git_hub_projetwebbdd_startcode_2019_2020_src_view_codaa29f657e39a46f17d491995db9c63a.html", null ],
+    [ "Vegas Background jQuery Plugin", "md__c_1__users_basti__one_drive__documents__git_hub_projetwebbdd_startcode_2019_2020_src_view_co650750fdb30066cca77d0a09eeaf9bce.html", null ],
+    [ "README", "md__c_1__users_basti__one_drive__documents__git_hub_projetwebbdd_startcode_2019_2020_src_view_coeb0873b645491c006fa14aba5009efea.html", null ],
+    [ "Vegas Background jQuery Plugin", "md__c_1__users_basti__one_drive__documents__git_hub_projetwebbdd_startcode_2019_2020_src_view_co8d015148e4c8336841a72e55353700c4.html", null ],
     [ "Data Structures", "annotated.html", [
       [ "Data Structures", "annotated.html", "annotated_dup" ],
       [ "Data Structure Index", "classes.html", null ],
